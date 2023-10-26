70bfe11c6d9723abe07ef22659045ce8bfe8e8cd
Naveenkumar K <naveenkumar.karuppusamy@ducenIT.com>
2023-10-26 11:18:53 +0530

    Initial commit

---
+++ src/main/java/com/DockerIntegration/DockerIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.DockerIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello.........Application Started");
+		SpringApplication.run(DockerIntegrationApplication.class, args);
+	}
+
+}
