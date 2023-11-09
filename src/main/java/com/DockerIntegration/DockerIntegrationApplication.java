4b265447205925f152ba5ce469935e84e622cc53
Naveenkumar K <naveenkumar.karuppusamy@ducenIT.com>
2023-11-09 11:53:26 +0530

    INCLUDING API

--- src/main/java/com/DockerIntegration/DockerIntegrationApplication.java
+++ src/main/java/com/DockerIntegration/DockerIntegrationApplication.java
@@ -2,6 +2,8 @@ package com.DockerIntegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DockerIntegrationApplication {
@@ -10,5 +12,11 @@ public class DockerIntegrationApplication {
 		System.out.println("Hello.........Application Started");
 		SpringApplication.run(DockerIntegrationApplication.class, args);
 	}
-
+	@RestController
+	public static class DockerMessageController {
+		@GetMapping("/messages")
+		public String getMessage() {
+			return "Hello from Docker!";
+		}
+	}
 }
